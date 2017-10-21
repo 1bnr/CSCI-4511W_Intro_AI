c5fae55e87261c6663a516a6a3163b0f164b49e8
1bnr <9zabner@gmail.com>
2017-10-21 17:24:41 -0500

    all tests working

--- nQueens.py
+++ nQueens.py
@@ -16,40 +16,42 @@ def fitness(q):
     return non_attacking
 
 def ga_nQueens(nXn, fit_thresh):
-    print("runnning")
     # Queens Problem
     gene_pool = range(nXn)
     population = init_population(100, gene_pool, nXn)
     solution = genetic_algorithm(population, fitness, gene_pool=gene_pool, f_thres=fit_thresh)
-    print(solution)
+#    print(solution)
     print('fitness: %s / %s, %% %.2f' %(fitness(solution), fit_thresh ,100 * float(fitness(solution)) / fit_thresh))
 
-def dfts_nQueens(nXn):
+def dfts_nQueens(nXn, fit_thresh):
     solution = depth_first_tree_search(NQueensProblem(nXn))
-    print(solution.state)
+    #print(solution.state)
+    #print('fitness: %s / %s, %% %.2f' %(fitness(solution.state), fit_thresh ,100 * float(fitness(solution.state)) / fit_thresh))
 
-def bfts_nQueens(nXn):
+def bfts_nQueens(nXn, fit_thresh):
     solution = breadth_first_tree_search(NQueensProblem(nXn))
-    print(solution.state)
+    #print(solution.state)
+    #print('fitness: %s / %s, %% %.2f' %(fitness(solution.state), fit_thresh ,100 * float(fitness(solution.state)) / fit_thresh))
 
 
-sizes = [8, 10, 12, 13, 14]
+sizes = [8, 10, 12, 13, 14, 15, 16, 17, 18, 20]
 
 if __name__ == '__main__':
-#    for n in sizes:
-#        mytimer(dfts_nQueens(n), 1)
-#    for n in sizes:
-#        mytimer(bfts_nQueens(n), 1)
-    for n in sizes:
-        t = timeit.timeit('dfts_nQueens(n)' 'from __main__ import dfts_nQueens, sizes, n', number=1)
-        print("runtime average: %s" % (t))
-    for n in sizes:
-        t = timeit.timeit('bfts_nQueens(n)' 'from __main__ import dfts_nQueens, sizes, n', number=1)
-        print("runtime average: %s" %(t))
+    # test the gentic algorithm
+    print("Genetic Algorithm Tree Search Test 10x each n x n")
     for n in sizes:
         t = timeit.timeit('ga_nQueens(n,sum(range(n)))', 'from __main__ import ga_nQueens, sizes, n', number=10)
-        print("runtime average: %s" %(t/10))
-#        mytimer(ga_nQueens(n,sum(range(n))), 1)
-#    dfts_nQueens(19)
-#    bfts_nQueens(12)
-#    ga_nQueens(10, 45)
+        print("%s x %s" %(n, n))
+        print("runtime average: " + "{0:.6f}".format(round(t/10,6)))
+    #test depth_first_tree_search
+    print("Depth First Tree Search Test 1x each n x n")
+    for n in sizes:
+        t = timeit.timeit('dfts_nQueens(n,sum(range(n)))', 'from __main__ import dfts_nQueens, sizes, n', number=1)
+        print("%s x %s" %( n, n))
+        print("%.6f" %t)
+    #test breadth_first_tree_search
+    print("Breadth First Tree Search Test 1x each n x n")
+    for n in sizes:
+        t = timeit.timeit('bfts_nQueens(n,sum(range(n)))', 'from __main__ import bfts_nQueens, sizes, n', number=1)
+        print("%s x %s" %(n, n))
+        print("%.6f" %t)
